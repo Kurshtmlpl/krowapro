70390fdf0c0e8d16709db38262615f024971d5c6
start.vaadin.com <no-reply@vaadin.com>
2022-09-14 19:52:51 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/index/IndexView.java
@@ -0,0 +1,35 @@
+package com.example.application.views.index;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Index")
+@Route(value = "index", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class IndexView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public IndexView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
---
+++ src/main/java/com/example/application/views/ttmdocumentupload/TtmdocumentuploadView.java
@@ -0,0 +1,31 @@
+package com.example.application.views.ttmdocumentupload;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Ttmdocumentupload")
+@Route(value = "ttmdocumentupload", layout = MainLayout.class)
+public class TtmdocumentuploadView extends VerticalLayout {
+
+    public TtmdocumentuploadView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
